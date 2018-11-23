b4108c3d0b01754441c7328663be2d808b02085e
Gregg Christofferson <25208248+GREGG-CHRISTOFFERSON@users.noreply.github.com>
2018-11-23 00:00:15 -0800

    adding my solutions to the challenges

---
+++ ch05/05_05/multiplication.php
@@ -0,0 +1,36 @@
+<?php $num_columns = 12; // number of columns in table ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Challenge: using loops</title>
+    <link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<h1>Multiplication Table</h1>
+<table>
+    <tr>
+        <th>&nbsp;</th>
+        <?php
+        // create the table column headers
+        for ($i = 1; $i < $num_columns + 1; $i++) :
+           echo "<th>$i</th>";
+        endfor;
+        ?>
+    </tr>
+        <?php
+        // create the table rows
+        for ($i = 1; $i < $num_columns + 1; $i++) :
+            echo '<tr>' .
+                    "<th>$i</th>";
+            // create the table columns for each row        
+            for ($j = 1; $j < $num_columns + 1; $j++) :
+                echo '<td>' . $i * $j . '</td>';
+            endfor;
+            echo '</tr>';
+        endfor;
+        ?>
+    </tr>
+</table>
+</body>
+</html>
